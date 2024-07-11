9ac1efd8f7cb45dd4c7162805a3b2d9630ad8970
Alexander Happel <Alex.happel90@gmail.com>
2024-07-11 14:10:15 -0400

    refactored connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,15 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Note: Setting this to false allows for self-signed certificates
+      },
+    },
   }
 );
 
